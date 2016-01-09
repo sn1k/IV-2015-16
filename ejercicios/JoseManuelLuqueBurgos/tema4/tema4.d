4adbfae868b8993cdaf69d09cf9c5c27f7d94b11
José Manuel Luque Burgos <luqueburgosjm@gmail.com>
2016-01-09 14:23:02 +0100

    Eje2

--- ejercicios/JoseManuelLuqueBurgos/tema4/tema4.d
+++ ejercicios/JoseManuelLuqueBurgos/tema4/tema4.d
@@ -47,4 +47,64 @@ Optional dependencies for lxc
     lua-alt-getopt: lxc-top
 ```
 
-Ejercicio 2: Comprobar qué interfaces puente se han creado y explicarlos.
+## Ejercicio 2
+### Comprobar qué interfaces puente se han creado y explicarlos.
+
+Creamos el contenedor con la orden: `sudo lxc-create -t ubuntu -n ubuntu-box`
+
+Tras la instalación nos aparece lo siguiente:
+
+```
+Generation complete.
+Setting up libdevmapper1.02.1 (2:1.02.48-4ubuntu7.4) ...
+Setting up dmsetup (2:1.02.48-4ubuntu7.4) ...
+update-initramfs: deferring update (trigger activated)
+Processing triggers for libc-bin ...
+ldconfig deferred processing now taking place
+Processing triggers for initramfs-tools ...
+Processing triggers for resolvconf ...
+invoke-rc.d: policy-rc.d denied execution of start.
+Download complete
+Copy /var/cache/lxc/precise/rootfs-amd64 to /var/lib/lxc/ubuntu-box/rootfs ...
+Copying rootfs to /var/lib/lxc/ubuntu-box/rootfs ...
+Generating locales...
+  en_GB.UTF-8... up-to-date
+Generation complete.
+Creating SSH2 RSA key; this may take some time ...
+Creating SSH2 DSA key; this may take some time ...
+Creating SSH2 ECDSA key; this may take some time ...
+Timezone in container is not configured. Adjust it manually.
+
+##
+# The default user is 'ubuntu' with password 'ubuntu'!
+# Use the 'sudo' command to run tasks as root in the container.
+##
+
+sudo lxc-create -t ubuntu -n ubuntu-box  152.69s user 24.34s system 17% cpu 16:26.51 total
+
+```
+
+Lo arrancamos con: `sudo lxc-start -n ubuntu-box`
+
+Ahora en el host ejecutamos ifconfig y vemos que hay dos nuevos interfaces:
+
+```
+lxbr0:
+        inet 10.0.3.1  netmask 255.255.255.0  broadcast 0.0.0.0
+        inet6 fe80::225:d3ff:fe63:a78e  prefixlen 64  scopeid 0x20<link>
+        ether fe:25:d3:63:a7:8e  txqueuelen 1000
+        RX packets 6115  bytes 3653012 (3.4 MiB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 2998  bytes 486081 (474.6 KiB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+vethSNB9QG:
+        ether fe:25:d3:63:a7:8e  txqueuelen 1000
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+```
+
+Uno sirve para que la máquina creada tenga acceso a internet mientras que la segunda sirve para tener acceso entre varias instancias de contenedores.
